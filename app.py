45142c61fb6261440b15da5f0a1baaea07c34dbd
natalieybl <94332856+natalieybl@users.noreply.github.com>
2024-08-29 16:17:33 +0000

    code issue on if statement

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ app=Flask(__name__)
 def index():
   return render_template('index.html')
 
-  if __name__=='__main__':
-    #app.run()
-    app.run(host='127.0.0.1', port=5000, debug=True)
+if __name__=='__main__':
+    app.run()
+    #app.run(host='127.0.0.1', port=5000, debug=True)
